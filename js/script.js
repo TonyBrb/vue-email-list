5cc308cdddd26d013c4c9199db602e39db1c35c7
TonyBrb <anthonybarbagallo95@gmail.com>
2021-11-22 16:25:21 +0100

    provo se l'email viene presa dal response

--- js/script.js
+++ js/script.js
@@ -4,14 +4,28 @@ const app = new Vue({
   el: '#app',
 
   data: {
-    saluto: 'ciao',
+    eMail:[]
   },
 
   methods:{
+    getMail(){
+        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) =>{
 
-  },
+          console.log('response.data',response.data);
+          const data = response.data;
+          const mail = data.response;
+          console.log(mail);
+          this.eMail.push(mail);
+
+        })
+        
+      }
+
+    },
 
   mounted(){
-
+    this.getMail();
   }
 })
